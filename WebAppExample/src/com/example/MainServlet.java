e25b5f8c614fcf9f34d65475c0b3c721b579d13e
superloboda <ann.loboda@gmail.com>
2015-12-10 12:19:40 +0200

    add web app example (lection 2)

---
+++ WebAppExample/src/com/example/MainServlet.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.dto.AnswerDTO;
+
+public class MainServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("date", new Date());
+		req.getRequestDispatcher("home.jsp").forward(req, resp);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String language = req.getParameter("language");
+		AnswerDTO answer = new AnswerDTO(name, language);
+		List<String> errors = validateForm(answer);
+		if (!errors.isEmpty()) {
+			req.setAttribute("answer", answer);
+			req.setAttribute("errors", errors);
+			req.getRequestDispatcher("home.jsp").forward(req, resp);
+		} else {
+			req.setAttribute("name", name);
+			req.getRequestDispatcher("answer.jsp").forward(req, resp);
+		}
+	}
+
+	private List<String> validateForm(AnswerDTO answer) {
+		List<String> errors = new ArrayList<>();
+
+		if (answer.getName() == null || answer.getName().isEmpty()) {
+			errors.add("Please, input your name");
+		}
+
+		if (answer.getLanguage() == null || answer.getLanguage().isEmpty()) {
+			errors.add("Please, input your language");
+		}
+		return errors;
+	}
+}
