fcba60fcbbb5ce949cecb46903179f2cf9e0c0e9
superloboda <ann.loboda@gmail.com>
2015-12-24 00:48:49 +0200

    fix with validation messages

--- WebAppExample/WebContent/resources/js/dev.js
+++ WebAppExample/WebContent/resources/js/dev.js
@@ -13,6 +13,11 @@ $(document).ready(function() {
 	}
 	addEventListener();
 	
+	function clearValidation(){
+		$('span.error').remove();
+		$('.form-group').removeClass('has-error');
+	}
+	
 	$(document).on( "click", ".add-answer",
 			function() {
 				$.ajax({
@@ -21,20 +26,24 @@ $(document).ready(function() {
 					data : $("#answer-form").serialize(),
 					success : function(record) {
 						console.log(record);
-						$('span.error').remove();
-						$('.form-group').removeClass('has-error');
+						clearValidation();
+						// append received record to the end of the table
 						var name_td = "<td>" + record.name + "</td>" ;
 						var language_td = "<td>" + record.language + "</td>" ;
 						var remove_td = "<td> <a id=" + record.id + " class=\"remove-answer\">remove</a> </td>";
 						$('#content tr:last').after("<tr id=" + record.id + ">" + name_td + language_td + remove_td + "</tr>");
+						
+						//reset form with last input
 						$('#answer-form').trigger("reset");
 						addEventListener();
 					},
 					error : function(data) {
 						console.log(data);
+						clearValidation();
 						$.each(data.responseJSON, function(key, value) {
 							console.log(key);
 							console.log(value);
+							//append error message to the input field and add has-error class 
 							$('#form-' + key).addClass("has-error");
 							$('#' + key + ".col-sm-8").append("<span class='error'>" + value + "</span>");
 						});
